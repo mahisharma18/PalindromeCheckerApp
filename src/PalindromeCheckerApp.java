4a3d358aaba60b64a93789765f0b70c6b507bd13
mahisharma18 <shmahi.0118@gmail.com>
2026-03-02 10:13:32 +0530

    UC3: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,25 +2,22 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "madam";   // Hardcoded string
-        boolean isPalindrome = true;
+        String input = "level";   // Hardcoded string
+        String reversed = "";
 
-        // Loop only till half of the string length
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        // Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
         }
 
-        System.out.println("Input String: " + input);
+        System.out.println("Original String: " + input);
+        System.out.println("Reversed String: " + reversed);
 
-        if (isPalindrome) {
+        if (input.equals(reversed)) {
             System.out.println("Result: The string is a palindrome.");
         } else {
             System.out.println("Result: The string is NOT a palindrome.");
         }
     }
 }
-
 
