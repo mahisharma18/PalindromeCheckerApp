391ccdd100eef45f9c60d133f5b950a1b4c65d5c
mahisharma18 <your GitHub email>
2026-03-25 09:28:27 +0530

    UC9: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,26 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        // Check palindrome
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println("Is Palindrome?: true");
+        } else {
+            System.out.println("Is Palindrome?: false");
+        }
+
+        sc.close();
+    }
 }
