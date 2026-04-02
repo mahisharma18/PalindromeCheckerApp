48ce01377bd1bfc026cb83116869db88a1a5fc5a
mahisharma18 <shmahi.0118@gmai.com>
2026-04-02 09:13:45 +0530

    02UC5: Display Consist Summary

--- src/PalindromeCalculator.java
+++ src/PalindromeCalculator.java
@@ -1,2 +1,72 @@
-public class PalindromeCalculator {
+import java.util.*;
+
+// CLASS 1: Reservation
+class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+// CLASS 2: BookingRequestQueue
+class BookingRequestQueue {
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+    }
+
+    public Reservation getNextRequest() {
+        return requestQueue.poll();
+    }
+
+    public boolean hasPendingRequests() {
+        return !requestQueue.isEmpty();
+    }
+}
+
+// CLASS 3: Main Class
+public class UseCase5BookingRequestQueue {
+
+    public static void main(String[] args) {
+
+        // Header
+        System.out.println("Booking Request Queue");
+
+        // Initialize queue
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        // Create booking requests
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vanmathi", "Suite");
+
+        // Add requests to queue
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+
+        // Process requests (FIFO)
+        while (bookingQueue.hasPendingRequests()) {
+            Reservation r = bookingQueue.getNextRequest();
+            System.out.println("Processing booking for "
+                    + r.getGuestName() + " - Room Type: "
+                    + r.getRoomType());
+        }
+    }
 }
